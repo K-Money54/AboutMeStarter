1f25bab45eaaefe227531c8f35c15e8b2655d0da
kayden.dunlop573@sccdsb.net <kayden.dunlop573@sccdsb.net>
2020-09-22 14:10:47 -0400

    started the program

--- AboutMe.java
+++ AboutMe.java
@@ -1,24 +1,22 @@
 /*
- * Programmer:
- * Date:
- * Purpose:
+ * Programmer: Kayden Dunlop
+ * Date: september 22, 2020
+ * Purpose: to say my name, age and then let time pass, and say what it is now.
  */
 
 public class AboutMe {
     public static void main (String [] args) {
-        // Declaring the variables
-        // TODO: Write the code the declares the variables
-
-        // Print the values
+    		String myName = "Kayden"
+    				
+    				
         System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
 
         // Change the variables
-        // TODO: Write the code that changes the variables
 
         // Print the new values
         System.out.println("Some time passes...");
-        // TODO: Write the last two print statements
+  
     }
 }
