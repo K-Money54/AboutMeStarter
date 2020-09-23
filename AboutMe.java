f4d9e5fd7dcc634309bc321d3a53842b39cc2892
kayden.dunlop573@sccdsb.net <kayden.dunlop573@sccdsb.net>
2020-09-23 10:52:59 -0400

    made the varables

--- AboutMe.java
+++ AboutMe.java
@@ -1,22 +1,27 @@
 /*
  * Programmer: Kayden Dunlop
- * Date: september 22, 2020
+ * Date: September 22, 2020
  * Purpose: to say my name, age and then let time pass, and say what it is now.
  */
 
 public class AboutMe {
     public static void main (String [] args) {
-    		String myName = "Kayden"
-    				
+    		String myName = "Kayden";
+    		int myAge = 16;	
+    		double myHeightInMeters = 1.82;
     				
         System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
-        System.out.println("My height is " + myHeightInMetres + " m.");
+        System.out.println("My height is " + myHeightInMeters + " m.");
 
         // Change the variables
-
+        	myAge = 18;
+        	myHeightInMeters = 2;
         // Print the new values
+        	
         System.out.println("Some time passes...");
-  
+        System.out.println("I am now " + myAge + " years old.");
+        System.out.println("My height is now " + myHeightInMeters + " m.");
+        
     }
 }
